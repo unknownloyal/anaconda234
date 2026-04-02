58a0b85dee25cbebe15a8cb3e6c6b7dd65055074
unknownloyal <unknownloyal910@gmail.com>
2026-04-02 08:10:10 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+
+A = load_digits().images[0]
+
+U, S, Vt = np.linalg.svd(A, False)
+
+def rec(k):
+    return U[:,:k] @ np.diag(S[:k]) @ Vt[:k,:]
+
+imgs = [A, rec(5), rec(10), rec(15)]
+titles = ["Original", "k=5", "k=10", "k=15"]
+
+for i in range(4):
+    plt.subplot(2,2,i+1)
+    plt.imshow(imgs[i], cmap='gray')
+    plt.title(titles[i])
+    plt.axis('off')
+
+plt.show()
+
+print("Original:", A.size)
+for k in [5,10,15]:
+    print(f"k={k}:", U[:,:k].size + S[:k].size + Vt[:k,:].size)
